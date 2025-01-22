f4dc5a86b155ffde1b6ab5dca4f288b86ef18f43
artem <artemka77752@gmail.com>
2025-01-23 02:50:15 +0300

    updates

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new CatchEverythingFilter(app.get(HttpAdapterHost)));
   app.enableCors({
-    // // origin: ['https://amraex.store']
-    // methods: 'GET,POST,PUT,DELETE',
-    // allowedHeaders: 'Content-Type, Authorization',
-    // credentials: true,
+    origin: ['https://vuzcrmplus.store/'],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
 }
