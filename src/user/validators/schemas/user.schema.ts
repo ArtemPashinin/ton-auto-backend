89fdf41508f0c49485594d4043bd2dee491334ff
artem <artemka77752@gmail.com>
2025-01-23 02:47:08 +0300

    updates

--- src/user/validators/schemas/user.schema.ts
+++ src/user/validators/schemas/user.schema.ts
@@ -2,9 +2,9 @@ import * as Joi from 'joi';
 
 export const userSchema = Joi.object({
   user_id: Joi.number().required(),
-  username: Joi.string().optional(),
-  first_name: Joi.string().optional(),
-  last_name: Joi.string().optional(),
+  username: Joi.string().optional().allow(''),
+  first_name: Joi.string().optional().allow(''),
+  last_name: Joi.string().optional().empty().allow(''),
   phone: Joi.string().optional(),
   language_code: Joi.string().optional(),
   city_id: Joi.number().required(),
