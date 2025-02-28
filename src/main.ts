9cffd6b2e71078d09e80d4ec3bf91403b63a834a
artem <artemka77752@gmail.com>
2025-02-28 13:10:22 +0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const environment = process.env.BOT_ENVIRONMENT;
-
+  console.log(environment);
   // Условие для настройки CORS
   if (environment === 'prod') {
+    console.log('this');
     app.enableCors({
-      origin: ['*'],
+      origin: 'https://vuzcrmplus.store',
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: 'Content-Type, Authorization',
       credentials: true,
