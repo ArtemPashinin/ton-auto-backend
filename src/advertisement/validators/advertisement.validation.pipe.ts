ee9de4af7dae9c87dc681ad7d43ebf03a90b0440
Artem <artem@MacBook-Air-Artem.local>
2025-03-03 16:30:30 +0300

    updated filter

--- src/advertisement/validators/advertisement.validation.pipe.ts
+++ src/advertisement/validators/advertisement.validation.pipe.ts
@@ -12,7 +12,6 @@ export class AdvertisementValidationPipe implements PipeTransform {
     const { error } = this.schema.validate(value, {
       abortEarly: false,
     });
-    console.log(error)
     if (error) {
       throw new HttpException(error.details[0].message, HttpStatus.BAD_REQUEST);
     }
