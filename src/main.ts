76581fcc0de9b1b2d477c70486eb451d2a17681c
artem <artemka77752@gmail.com>
2025-03-09 18:38:31 +0300

    update dockerfile

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const environment = process.env.BOT_ENVIRONMENT;
   if (environment === 'prod') {
     app.enableCors({
-      origin: 'https://tonauto.app',
+      origin: ['https://tonauto.app', 'https://vuzcrmplus.store/'],
       methods: 'GET,POST,PUT,DELETE',
       allowedHeaders: 'Content-Type, Authorization',
       credentials: true,
