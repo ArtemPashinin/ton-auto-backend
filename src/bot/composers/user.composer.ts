364dcab027aa736f5fd01e52e4844462c29f1107
Артём Пашинин <a.pashinin@os-tech.ru>
2025-04-21 21:04:20 +0300

    fixes

--- src/bot/composers/user.composer.ts
+++ src/bot/composers/user.composer.ts
@@ -31,10 +31,10 @@ export class UserComposer implements OnModuleInit {
 
   private async start(ctx: Context): Promise<void> {
     await ctx.reply(
-      `🚗 Welcome to Drivio!
+      `Welcome to Drivio!
 Looking for a new ride or ready to sell your car? I'm here to make it quick and simple!
--- Browse listings for new and used cars
--- Post your car for sale in just a few steps
+— Browse listings for new and used cars
+— Post your car for sale in just a few steps
 Let’s find your perfect match`,
       { reply_markup: this.openWebAppButton },
     );
